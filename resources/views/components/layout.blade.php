049ad1eb90157b4a8ada42227a46543d5fd2080d
Mr-Explorer142 <tahmidalamtamim142@gmail.com>
2025-09-13 13:10:57 +0600

    Add Authentication features to the project

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
- <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="UTF-8">
@@ -21,7 +21,7 @@
             setTimeout(() => {
                 const flash = document.getElementById('flash');
                 if (flash) {
-                    flash.style.opacity = '0';  // fade out
+                    flash.style.opacity = '0'; // fade out
                     setTimeout(() => flash.remove(), 1000);
                 }
             }, 3000); // disappears after 3 seconds
@@ -32,9 +32,25 @@
     {{-- Header Section --}}
     <header>
         <nav>
-            <h1 class="font-bold text-2xl">Explorer's Network</h1>
-            <a href="{{ route('explorers.index') }}">All Explorers</a>
-            <a href="{{ route('explorers.create') }}">Create New Explorer</a>
+            <h1 class="font-bold text-2xl">
+                <a href="{{ route('explorers.index') }}">Explorers' Net</a>
+            </h1>
+
+            @guest
+                <a href="{{ route('show.login') }}" class="btn">Login</a>
+                <a href="{{ route('show.register') }}" class="btn">Register</a>
+            @endguest
+
+            @auth
+                <span class="border-r-2 border-red-500 pr-2">
+                    Hi there, {{ Auth::user()->name }}
+                </span>
+                <a href="{{ route('explorers.create') }}">Create New Explorer</a>
+                <form action="{{ route('logout') }}" method="POST" class="m-0">
+                    @csrf
+                    <button type="submit" class="btn">Logout</button>
+                </form>
+            @endauth
         </nav>
     </header>
     {{-- Main Section --}}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExplorerController;
 
@@ -7,19 +8,33 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/explorers', [ExplorerController::class, 'index'])->name('explorers.index');
+// Logout Route
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/explorers/create', [ExplorerController::class, 'create'])->name('explorers.create');
+// Signup and Login Routes
+Route::middleware('guest')->controller(AuthController::class)->group(function () {
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+});
 
-// Route::get('/explorers/{id}', [ExplorerController::class, 'show'])->name('explorers.show');
-Route::get('/explorers/{explorer}', [ExplorerController::class, 'show'])->name('explorers.show');
+// CRUD Routes
+Route::middleware('auth')->controller(ExplorerController::class)->group(function () {
+    Route::get('/explorers', [ExplorerController::class, 'index'])->name('explorers.index');
 
-Route::post('/explorers', [ExplorerController::class, 'store'])->name('explorers.store');
+    Route::get('/explorers/create', [ExplorerController::class, 'create'])->name('explorers.create')->middleware('auth');
 
-// Route::delete('/explorers/{id}', [ExplorerController::class, 'destroy'])->name('explorers.destroy');
+    // Route::get('/explorers/{id}', [ExplorerController::class, 'show'])->name('explorers.show');
+    Route::get('/explorers/{explorer}', [ExplorerController::class, 'show'])->name('explorers.show');
 
-Route::delete('/explorers/{explorer}', [ExplorerController::class, 'destroy'])->name('explorers.destroy');
+    Route::post('/explorers', [ExplorerController::class, 'store'])->name('explorers.store');
 
-Route::get('/explorers/{explorer}/edit', [ExplorerController::class, 'edit'])->name('explorers.edit');
+    // Route::delete('/explorers/{id}', [ExplorerController::class, 'destroy'])->name('explorers.destroy');
 
-Route::put('/explorers/{explorer}', [ExplorerController::class, 'update'])->name('explorers.update');
+    Route::delete('/explorers/{explorer}', [ExplorerController::class, 'destroy'])->name('explorers.destroy');
+
+    Route::get('/explorers/{explorer}/edit', [ExplorerController::class, 'edit'])->name('explorers.edit');
+
+    Route::put('/explorers/{explorer}', [ExplorerController::class, 'update'])->name('explorers.update');
+});
